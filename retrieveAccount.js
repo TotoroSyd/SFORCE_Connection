6df1700a324904c62dfd127a471ebd80c3cbde6b
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2022-01-26 21:40:05 +1100

    Create Promise in child. Async await in caller. fix undefined issue in browser

--- retrieveAccount.js
+++ retrieveAccount.js
@@ -1,1 +1,23 @@
-export default function () {}
+// Use promise
+const retrieveAccount = (conn) => {
+  const soql = `SELECT Id, Name, CreatedById, OwnerId FROM Account WHERE CreatedById='0055g00000ArZNIAA3' LIMIT 100`;
+  // Use Promise
+  return new Promise((resolve, reject) => {
+    let accounts = [];
+    conn.query(soql, (err, result) => {
+      if (result.records.length === 0) {
+        // https://javascript.info/try-catch#throw-operator
+        reject(new Error("Session not found"));
+      } else if (err) {
+        reject(err);
+      } else {
+        accounts = result.records.map((record) => {
+          return { id: record.Id, name: record.Name };
+        });
+        resolve(accounts);
+      }
+    });
+  });
+};
+
+module.exports = retrieveAccount;
