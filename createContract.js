2cc2db99002d812871547a0f291785dc9f0c09c6
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2022-05-10 22:50:01 +1000

    Check return customer condition in createcontract

--- createContract.js
+++ createContract.js
@@ -1,19 +1,27 @@
-// Use promise
-const createContract = (conn) => {
-  // Single contract record
-  conn.sobject("Contracts").create({
-    AccountId: "0015g00000GmYPBAA3",
-    OwnerId: "0055g00000ArZNIAA3",
-    StartDate: "2022-03-26",
-    EndDate: "2022-03-31",
-  }),
-    function (err, res) {
-      if (err || !res.success) {
-        return console.error(err, res);
-      }
-    };
-  console.log("Created record id : " + res.id);
-};
+export default function createContract(conn, isReturnCust) {
+  // Check if it is a return customer
+  if (isReturnCust) {
+    // create contract record
+    insertContract();
+  } else {
+    // Create new contact if it is a new customer
+    // create contract record
+    insertContract();
+  }
 
-// Export module
-module.exports = createContract;
+  function insertContract() {
+    // Single contract record
+    conn.sobject("Contracts").create({
+      AccountId: "0015g00000GmYPBAA3",
+      OwnerId: "0055g00000ArZNIAA3",
+      StartDate: "2022-03-26",
+      EndDate: "2022-03-31",
+    }),
+      function (err, res) {
+        if (err || !res.success) {
+          return console.error(err, res);
+        }
+      };
+    console.log("Created record id : " + res.id);
+  }
+}
