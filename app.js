954010adaabd133f436b1681282cbdc53b1a6408
PhoebeNguyen <phoebe.ngsyd@gmail.com>
2022-06-23 08:29:37 +1000

    Reroute url

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // Adding library
 const express = require("express");
 const cors = require("cors");
-const router = require("./routes");
+const rootRouter = require("./routes/rootRoute");
 require("dotenv").config();
 
 // Create express app
@@ -10,9 +10,6 @@ const app = express();
 // Use express modules
 app.use(cors());
 app.use(express.json());
-
-// Distribute requests to routes
-app.use("/", router);
 
 // port 3000 is being used by MochiMachi client side
 app.set("port", 3001);
@@ -25,13 +22,19 @@ if (!(SF_USERNAME && SF_PASSWORD && SF_TOKEN && SF_LOGIN_URL)) {
   process.exit(-1);
 }
 
+// Distribute requests to routes
+app.use("/", rootRouter);
+
 // ------Below is default set up for express----
 // Hanlde 404
 app.all("*", (req, res) => {
-  return res.status(404).send("Resource not found");
+  return res.status(404).send("Damn! Resource not found");
 });
 
 // Express server listening
-app.listen(app.get("port"), () => {
+app.listen(app.get("port"), (err) => {
+  if (err) {
+    return console.log("ERROR", err);
+  }
   console.log(`Server listening at port: ${app.get("port")}`);
 });
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const createContractRoute = require("./contract");
-const accountRoute = require("./account");
-const router = express.Router();
-
-router.get("/createContract", createContractRoute);
-router.get("/account", accountRoute);
-
-module.exports = router;
